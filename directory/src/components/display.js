d40f17763b7a786a17f827e300472b4f4606f792
Alex <achriscurtis@gmail.com>
2020-09-07 23:16:54 -0500

    minor changes

--- directory/src/components/display.js
+++ directory/src/components/display.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import data from '../db/data.json';
-// import Employee from './employee.js';
 
 function Employee(props) {
     return (
@@ -17,22 +16,21 @@ class Display extends Component {
     constructor(props) {
         super(props);
           this.state = {
-            originalData: data,
+            data: data,
             employees: data
           }
         }
       
-        sortByFirstName() {
-            const sortResult = (this.state.originalData).sort((a,b) => (a.first_name > b.first_name) ? 1 : -1)
+        sortByFirst() {
+            const sortResult = (this.state.data).sort((a,b) => (a.first_name > b.first_name) ? 1 : -1)
             this.setState({ employees: sortResult });
         }
       
         filterByRole(event) {
-          let employeesCopy = this.state.originalData.slice();
-          let filteredEmployees = employeesCopy.filter(employee =>
+          let employees = this.state.data.slice();
+          let filteredRole = employees.filter(employee =>
             employee.role_id.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()));
-      
-          this.setState({ employees: filteredEmployees });
+          this.setState({ employees: filteredRole });
         }
       
         render() {
@@ -41,7 +39,7 @@ class Display extends Component {
             <table className="table">
               <thead>
                 <tr>
-                  <th scope="col"><button onClick={() => this.sortByFirstName()}>First</button></th>
+                  <th scope="col"><button onClick={() => this.sortByFirst()}>First</button></th>
                   <th scope="col">Last</th>
                   <th scope="col">Role ID</th>
                   <td><input type="text" onChange={(event) => this.filterByRole(event)} />Search By Role Here</td>
